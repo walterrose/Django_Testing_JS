a6d4702a91cc9402f445eab288603f079a0584c8
walterrose <walterrose264@gmail.com>
2021-01-13 04:33:13 -0500

    Fixed configure of templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-
 from .import views
-# from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
-# router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
-#
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
